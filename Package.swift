e5864598db18fb7b5e30dfe569dc4722e696821b
Syo Ikeda <suicaicoca@gmail.com>
2016-10-21 13:55:18 +0900

    Update Nimble to v5.1.0
    https://github.com/Quick/Nimble/releases/tag/v5.1.0

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         #else
             return [
                 .Package(url: "https://github.com/antitypical/Result.git", majorVersion: 3, minor: 0),
-                .Package(url: "https://github.com/Quick/Nimble", majorVersion: 5, minor: 0),
+                .Package(url: "https://github.com/Quick/Nimble", majorVersion: 5, minor: 1),
                 .Package(url: "https://github.com/Quick/Quick", majorVersion: 0, minor: 10),
             ]
         #endif
