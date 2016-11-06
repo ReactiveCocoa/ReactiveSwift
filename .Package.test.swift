1a960d01a675631b4ca13392884e8036f492a218
Syo Ikeda <suicaicoca@gmail.com>
2016-11-06 22:00:13 +0900

    [SwiftPM] Loosen Result version constraint

--- .Package.test.swift
+++ .Package.test.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "ReactiveSwift",
     dependencies: [
-        .Package(url: "https://github.com/antitypical/Result.git", majorVersion: 3, minor: 0),
+        .Package(url: "https://github.com/antitypical/Result.git", majorVersion: 3),
         .Package(url: "https://github.com/Quick/Nimble", majorVersion: 5, minor: 0),
         .Package(url: "https://github.com/Quick/Quick", majorVersion: 0, minor: 10),
     ],
--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "ReactiveSwift",
     dependencies: [
-        .Package(url: "https://github.com/antitypical/Result.git", majorVersion: 3, minor: 0),
+        .Package(url: "https://github.com/antitypical/Result.git", majorVersion: 3),
     ],
     exclude: [
         "Sources/Deprecations+Removals.swift",
