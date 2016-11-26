88cab2165b79e84d0e937d569d1eeaad5e605452
Anders <anders@andersha.com>
2016-11-26 05:49:42 +0100

    Introduce `UnsafeAtomicState` a simple lock-free FSM.

--- Sources/Atomic.swift
+++ Sources/Atomic.swift
@@ -7,6 +7,123 @@
 //
 
 import Foundation
+#if os(macOS) || os(iOS) || os(tvOS) || os(watchOS)
+import MachO
+#endif
+
+/// Represents a finite state machine that can transit from one state to
+/// another.
+internal protocol AtomicStateProtocol {
+	associatedtype State: RawRepresentable
+
+	/// Try to transit from the expected current state to the specified next
+	/// state.
+	///
+	/// - parameters:
+	///   - expected: The expected state.
+	///
+	/// - returns:
+	///   `true` if the transition succeeds. `false` otherwise.
+	mutating func tryTransiting(from expected: State, to next: State) -> Bool
+}
+
+/// A simple, generic lock-free finite state machine.
+///
+/// - warning: `deinitialize` must be called to dispose of the consumed memory.
+internal struct UnsafeAtomicState<State: RawRepresentable>: AtomicStateProtocol where State.RawValue == Int32 {
+	internal typealias Transition = (expected: State, next: State)
+#if os(macOS) || os(iOS) || os(tvOS) || os(watchOS)
+	private let value: UnsafeMutablePointer<Int32>
+
+	/// Create a finite state machine with the specified initial state.
+	///
+	/// - parameters:
+	///   - initial: The desired initial state.
+	internal init(_ initial: State) {
+		value = UnsafeMutablePointer<Int32>.allocate(capacity: 1)
+		value.initialize(to: initial.rawValue)
+	}
+
+	/// Deinitialize the finite state machine.
+	internal func deinitialize() {
+		value.deinitialize()
+		value.deallocate(capacity: 1)
+	}
+
+	/// Compare the current state with the specified state.
+	///
+	/// - parameters:
+	///   - expected: The expected state.
+	///
+	/// - returns:
+	///   `true` if the current state matches the expected state. `false`
+	///   otherwise.
+	@inline(__always)
+	internal func `is`(_ expected: State) -> Bool {
+		return OSAtomicCompareAndSwap32Barrier(expected.rawValue,
+		                                       expected.rawValue,
+		                                       value)
+	}
+
+	/// Try to transit from the expected current state to the specified next
+	/// state.
+	///
+	/// - parameters:
+	///   - expected: The expected state.
+	///
+	/// - returns:
+	///   `true` if the transition succeeds. `false` otherwise.
+	@inline(__always)
+	internal func tryTransiting(from expected: State, to next: State) -> Bool {
+		return OSAtomicCompareAndSwap32Barrier(expected.rawValue,
+		                                       next.rawValue,
+		                                       value)
+	}
+#else
+	private let value: Atomic<Int32>
+
+	/// Create a finite state machine with the specified initial state.
+	///
+	/// - parameters:
+	///   - initial: The desired initial state.
+	internal init(_ initial: State) {
+		value = Atomic(initial.rawValue)
+	}
+
+	/// Deinitialize the finite state machine.
+	internal func deinitialize() {}
+
+	/// Compare the current state with the specified state.
+	///
+	/// - parameters:
+	///   - expected: The expected state.
+	///
+	/// - returns:
+	///   `true` if the current state matches the expected state. `false`
+	///   otherwise.
+	internal func `is`(_ expected: State) -> Bool {
+		return value.modify { $0 == expected.rawValue }
+	}
+
+	/// Try to transit from the expected current state to the specified next
+	/// state.
+	///
+	/// - parameters:
+	///   - expected: The expected state.
+	///
+	/// - returns:
+	///   `true` if the transition succeeds. `false` otherwise.
+	internal func tryTransiting(from expected: State, to next: State) -> Bool {
+		return value.modify { value in
+			if value == expected.rawValue {
+				value = next.rawValue
+				return true
+			}
+			return false
+		}
+	}
+#endif
+}
 
 final class PosixThreadMutex: NSLocking {
 	private var mutex = pthread_mutex_t()
