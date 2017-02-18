9b21c4938a84fc4fa11358435c4301f2a6821b39
Anders <anders@andersha.com>
2017-02-18 19:32:07 +0800

    Include API deprecations in SwiftPM builds.

--- .Package.test.swift
+++ .Package.test.swift
@@ -6,8 +6,5 @@ let package = Package(
         .Package(url: "https://github.com/antitypical/Result.git", versions: Version(3, 1, 0)..<Version(3, .max, .max)),
         .Package(url: "https://github.com/Quick/Quick", majorVersion: 1, minor: 1),
         .Package(url: "https://github.com/Quick/Nimble", majorVersion: 6),
-    ],
-    exclude: [
-        "Sources/Deprecations+Removals.swift",
     ]
 )
