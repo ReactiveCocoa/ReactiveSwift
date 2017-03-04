3487f19d61d5065aaa5c332e7a5bdd38a04bf688
Syo Ikeda <suicaicoca@gmail.com>
2017-03-04 22:42:22 +0900

    Update Result to 3.2.1 for Swift 3.1 compatibility on Linux

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "ReactiveSwift",
     dependencies: [
-        .Package(url: "https://github.com/antitypical/Result.git", versions: Version(3, 1, 0)..<Version(3, .max, .max)),
+        .Package(url: "https://github.com/antitypical/Result.git", versions: Version(3, 2, 1)..<Version(3, .max, .max)),
     ]
 )
