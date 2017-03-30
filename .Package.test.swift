615115a5c9925d08c31c02e7c877efb385b94add
Syo Ikeda <suicaicoca@gmail.com>
2017-03-30 21:41:16 +0900

    Update Nimble to 6.1.0

--- .Package.test.swift
+++ .Package.test.swift
@@ -5,6 +5,6 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/antitypical/Result.git", versions: Version(3, 2, 1)..<Version(3, .max, .max)),
         .Package(url: "https://github.com/Quick/Quick", majorVersion: 1, minor: 1),
-        .Package(url: "https://github.com/Quick/Nimble", majorVersion: 6),
+        .Package(url: "https://github.com/Quick/Nimble", majorVersion: 6, minor: 1),
     ]
 )
