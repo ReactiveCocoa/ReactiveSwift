91fdbef434b5e8dfd86f7a33a23c33f8663a3529
Syo Ikeda <suicaicoca@gmail.com>
2017-04-04 22:23:14 +0900

    [Package.swift] Add `isSwiftPackagerManagerTest` computed variable

--- Package.swift
+++ Package.swift
@@ -1,13 +1,17 @@
 import Foundation
 import PackageDescription
 
+var isSwiftPackagerManagerTest: Bool {
+    return ProcessInfo.processInfo.environment["SWIFTPM_TEST_ReactiveSwift"] == "YES"
+}
+
 let package = Package(
     name: "ReactiveSwift",
     dependencies: {
         var deps: [Package.Dependency] = [
             .Package(url: "https://github.com/antitypical/Result.git", versions: Version(3, 2, 1)..<Version(3, .max, .max)),
         ]
-        if ProcessInfo.processInfo.environment["SWIFTPM_TEST_ReactiveSwift"] == "YES" {
+        if isSwiftPackagerManagerTest {
             deps += [
                 .Package(url: "https://github.com/Quick/Quick.git", majorVersion: 1, minor: 1),
                 .Package(url: "https://github.com/Quick/Nimble.git", majorVersion: 6, minor: 1),
