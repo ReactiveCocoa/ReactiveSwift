cc3313437a3d75031f3e8e5d2a4bc0302f35fdac
Sho Ikeda <suicaicoca@gmail.com>
2017-11-26 16:35:25 +0900

    [CI] Use Xcode 9.1 image and Swift 4.0.2 (#566)
    * [CI] Use Xcode 9.1 image and Swift 4.0.2
    
    * Update Nimble to 7.0.3 for Swift 4.0.2 compatibility on Linux
    
    * [CI] Update some xcodebuild destinations

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/antitypical/Result.git", from: "3.2.1"),
         .package(url: "https://github.com/Quick/Quick.git", from: "1.2.0"),
-        .package(url: "https://github.com/Quick/Nimble.git", from: "7.0.2"),
+        .package(url: "https://github.com/Quick/Nimble.git", from: "7.0.3"),
     ],
     targets: [
         .target(name: "ReactiveSwift", dependencies: ["Result"], path: "Sources"),
