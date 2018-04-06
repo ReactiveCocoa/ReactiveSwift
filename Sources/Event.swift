a7c5e4f688eae69695f1e83b4bf9d457fe460503
Charlotte Tortorella <charlotte@monadic.consulting>
2018-04-06 10:37:16 +1000

    Change variable naming

--- Sources/Event.swift
+++ Sources/Event.swift
@@ -864,11 +864,11 @@ extension Signal.Event {
 	
 	internal static func collect(every interval: DispatchTimeInterval, on scheduler: DateScheduler, skipEmpty: Bool, discardWhenCompleted: Bool) -> Transformation<[Value], Error> {
 		return { action, lifetime in
-			let values = Atomic<CollectEveryState<Value>>(.init(skipEmpty: skipEmpty))
+			let state = Atomic<CollectEveryState<Value>>(.init(skipEmpty: skipEmpty))
 			let d = SerialDisposable()
 			
 			d.inner = scheduler.schedule(after: scheduler.currentDate.addingTimeInterval(interval), interval: interval, leeway: interval * 0.1) { [weak d] in
-				let (currentValues, isCompleted) = values.modify { ($0.collect(), $0.isCompleted) }
+				let (currentValues, isCompleted) = state.modify { ($0.collect(), $0.isCompleted) }
 				if let currentValues = currentValues {
 					action(.value(currentValues))
 				}
@@ -886,11 +886,11 @@ extension Signal.Event {
 			return { event in
 				switch event {
 				case let .value(value):
-					values.modify { $0.values.append(value) }
+					state.modify { $0.values.append(value) }
 				case let .failed(error):
 					d.inner = scheduler.schedule { action(.failed(error)) }
 				case .completed where !discardWhenCompleted:
-					values.modify { $0.isCompleted = true }
+					state.modify { $0.isCompleted = true }
 				case .completed:
 					d.inner = scheduler.schedule { action(.completed) }
 				case .interrupted:
