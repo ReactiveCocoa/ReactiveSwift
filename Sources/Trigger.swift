770fa5d29416be00bb9efc8f84285709ea527a72
Marco Cancellieri <marco@cancellieri.de>
2018-04-10 18:36:27 +0200

    Introduce Trigger

---
+++ Sources/Trigger.swift
@@ -0,0 +1,40 @@
+import Foundation
+import Result
+
+/// A Trigger is a simple mechanism that can be used to when no explicit value is needed.
+public final class Trigger: BindingSource, BindingTargetProvider {
+	public typealias Value = ()
+	public typealias Error = NoError
+	
+	let (lifetime, token) = Lifetime.make()
+	let observer: Signal<Value, Error>.Observer
+	/// A signal that sends `Void` whenever the trigger is fired.
+	public var signal: Signal<Value, Error>
+	
+	public init() {
+		(self.signal, self.observer) = Signal<Value, Error>.pipe()
+	}
+	
+	public convenience init(capturing signal: Signal<Value, Error>) {
+		self.init()
+		signal.observe(observer)
+	}
+	
+	public convenience init<P: PropertyProtocol>(capturing property: P) where P.Value == Value {
+		self.init(capturing: property.signal)
+	}
+	
+	/// Fires the trigger.
+	public func fire() {
+		observer.send(value: ())
+	}
+	
+	// MARK: BindingSource
+	public var producer: SignalProducer<Value, Error> {
+		return SignalProducer(signal)
+	}
+	// MARK: BindingTargetProvider
+	public var bindingTarget: BindingTarget<Value> {
+		return BindingTarget(lifetime: lifetime) { [weak self] in self?.fire() }
+	}
+}
