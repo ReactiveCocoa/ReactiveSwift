73d7f9529e596dd9da98f3fe15d2f21073eb49a3
Leonid S. Usov <leonid@practi.net>
2018-08-18 12:56:32 +0300

    AsyncValue POC

--- ReactiveSwift.playground/Pages/Sandbox.xcplaygroundpage/Contents.swift
+++ ReactiveSwift.playground/Pages/Sandbox.xcplaygroundpage/Contents.swift
@@ -1,23 +1,191 @@
-/*:
- > # IMPORTANT: To use `ReactiveSwift.playground`, please:
- 
- 1. Retrieve the project dependencies using one of the following terminal commands from the ReactiveSwift project root directory:
-    - `git submodule update --init`
- **OR**, if you have [Carthage](https://github.com/Carthage/Carthage) installed
-    - `carthage checkout`
- 1. Open `ReactiveSwift.xcworkspace`
- 1. Build `ReactiveSwift-macOS` scheme
- 1. Finally open the `ReactiveSwift.playground`
- 1. Choose `View > Show Debug Area`
- */
 
 import ReactiveSwift
 import Foundation
 
-/*:
- ## Sandbox
- 
- A place where you can build your sand castles 🏖.
-*/
 
+//enum AsyncError<Error: Swift.Error>: Swift.Error {
+//    case rejected(Error)
+//    case interrupted
+//}
+
+protocol Awaitable {
+    associatedtype Value
+    associatedtype Error: Swift.Error
+    typealias AsyncResult = Result<Value, Error>
+    
+    func await(notify: @escaping (AsyncResult) -> Void) -> Disposable
+}
+
+protocol AsyncValueType: Awaitable {
+    
+    typealias ResolveCallback = (AsyncResult) -> Void
+    
+    init(_ resolver: @escaping (@escaping ResolveCallback, Lifetime) -> Void)
+    init(_ producer: SignalProducer<Value, Error>)
+
+}
+
+extension AsyncValueType {
+    init<A: Awaitable>(_ awaitable: A) where A.Value == Value, A.Error == Error  {
+        self.init { resolve, lifetime in
+            lifetime += awaitable.await(notify: resolve)
+        }
+    }
+    init(_ value: Value) {
+        self.init { resolve, _ in resolve(.success(value)) }
+    }
+    init(_ error: Error) {
+        self.init { resolve, _ in resolve(.failure(error)) }
+    }
+    init(_ result: Result<Value, Error>) {
+        self.init { resolve, _ in resolve(result) }
+    }
+}
+
+class AsyncValue<Value, Error: Swift.Error>: AsyncValueType {
+    
+    // without this swift segfaults :/
+    typealias AsyncResult = Result<Value, Error>
+    
+    enum State {
+        case pending
+        case resolved(AsyncResult)
+        
+        var asyncResult: AsyncResult? {
+            switch self {
+            case .pending: return nil
+            case .resolved(let result): return result
+            }
+        }
+    }
+    
+    private let state: Property<State>
+    
+    fileprivate init(state: Property<State>) {
+        self.state = state
+    }
+    
+    func await(notify: @escaping (AsyncResult) -> Void) -> Disposable {
+        return state.producer
+            .filterMap { $0.asyncResult }
+            .startWithValues(notify)
+    }
+    
+    required convenience init(_ resolver: @escaping (@escaping ResolveCallback, Lifetime) -> Void) {
+        let resolvingProducer = SignalProducer<State, NoError> { obs, life in
+            func resolve(_ result: Result<Value, Error>) {
+                switch result {
+                case .success(let value): obs.send(value: .resolved(.success(value)))
+                case .failure(let error): obs.send(value: .resolved(.failure(error)))
+                }
+                obs.sendCompleted()
+            }
+            resolver(resolve, life)
+        }
+        self.init(state: Property<State>(initial: .pending, then: resolvingProducer))
+    }
+    
+    required convenience init(_ producer: SignalProducer<Value, Error>) {
+        self.init { resolve, lifetime in
+            lifetime += producer.startWithResult(resolve)
+        }
+    }
+    
+    deinit {
+        print("\(type(of:self)) deinit")
+    }
+}
+
+class AsyncValueProducer<Value, Error: Swift.Error>: AsyncValueType {
+    
+    private let producer: SignalProducer<Value, Error>
+    
+    fileprivate init(producer: SignalProducer<Value, Error>) {
+        self.producer = producer
+    }
+    
+    func await(notify: @escaping (AsyncResult) -> Void) -> Disposable {
+        return producer.startWithResult(notify)
+    }
+    
+    required convenience init(_ resolver: @escaping (@escaping ResolveCallback, Lifetime) -> Void) {
+        let resolvingProducer = SignalProducer<Value, Error> { obs, life in
+            func resolve(_ result: Result<Value, Error>) {
+                switch result {
+                case .success(let value):
+                    obs.send(value: value)
+                    obs.sendCompleted()
+                case .failure(let error):
+                    obs.send(error: error)
+                }
+            }
+            resolver(resolve, life)
+        }
+        self.init(producer: resolvingProducer)
+    }
+    
+    required convenience init(_ producer: SignalProducer<Value, Error>) {
+        self.init(producer: producer.take(first: 1))
+    }
+    
+    deinit {
+        print("\(type(of:self)) deinit")
+    }
+}
+
+
+extension AsyncValueProducer {
+    func start() -> AsyncValue<Value, Error> {
+        return AsyncValue(self)
+    }
+}
+
+
+extension AsyncValueType {
+    func when(ready onReady: (@escaping (Value) -> Void), error onError: ((Error) -> Void)? = nil) -> Self {
+        return Self { (resolve, lifetime) in
+            lifetime += self.await { result in
+                switch result {
+                case .failure(let error): onError?(error)
+                case .success(let value): onReady(value)
+                }
+                resolve(result)
+            }
+        }
+    }
+    
+    func map<T: AsyncValueType>(_ transform: @escaping (Value) -> T) -> T where T.Error == Error {
+        return T { (resolve, lifetime) in
+            lifetime += self.await { result in
+                switch result {
+                case .failure(let error): resolve(.failure(error))
+                case .success(let value):
+                    lifetime += transform(value).await(notify: resolve)
+                }
+            }
+        }
+    }
+}
+
+
+
+var seed = 0
+
+let seedProducer = SignalProducer<Int, NoError> { obs, _ in
+    obs.send(value: seed)
+}
+
+
+let value = AsyncValueProducer(seedProducer)
+    .map {
+        // try changing the below between AsyncValue and AsyncValueProducer
+        AsyncValueProducer($0 + 10)
+    }
+    .when(ready: { print("resolved with value \($0)") })
+    
+print("incrementing seed")
+seed += 1
+
+print("awaiting for a result")
+    value.await(notify: { print("await notified with \($0)") })
 
