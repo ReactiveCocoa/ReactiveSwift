38b9f2858f886235f0e8bbd161d0b2a4a9331a72
Sho Ikeda <suicaicoca@gmail.com>
2019-01-06 22:42:46 +0900

    Update Result to 4.1.0 (#698)
    https://github.com/antitypical/Result/releases/tag/4.1.0

--- Package@swift-4.2.swift
+++ Package@swift-4.2.swift
@@ -7,7 +7,7 @@ let package = Package(
         .library(name: "ReactiveSwift", targets: ["ReactiveSwift"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/antitypical/Result.git", from: "4.0.0"),
+        .package(url: "https://github.com/antitypical/Result.git", from: "4.1.0"),
         .package(url: "https://github.com/Quick/Quick.git", from: "1.3.2"),
         .package(url: "https://github.com/Quick/Nimble.git", from: "7.3.1"),
     ],
