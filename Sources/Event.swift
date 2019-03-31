ad48733412aeae7042954a553f77fe6e6e8089c9
Yasuhiro Inami <inamiy@gmail.com>
2019-03-31 12:32:44 +0900

    Adjust indent

--- Sources/Event.swift
+++ Sources/Event.swift
@@ -2,120 +2,120 @@ import Result
 import Foundation
 import Dispatch
 
-	/// Represents a signal event.
+/// Represents a signal event.
+///
+/// Signals must conform to the grammar:
+/// `value* (failed | completed | interrupted)?`
+public enum Event<Value, Error: Swift.Error> {
+	/// A value provided by the signal.
+	case value(Value)
+
+	/// The signal terminated because of an error. No further events will be
+	/// received.
+	case failed(Error)
+
+	/// The signal successfully terminated. No further events will be received.
+	case completed
+
+	/// Event production on the signal has been interrupted. No further events
+	/// will be received.
 	///
-	/// Signals must conform to the grammar:
-	/// `value* (failed | completed | interrupted)?`
-	public enum Event<Value, Error: Swift.Error> {
-		/// A value provided by the signal.
-		case value(Value)
+	/// - important: This event does not signify the successful or failed
+	///              completion of the signal.
+	case interrupted
 
-		/// The signal terminated because of an error. No further events will be
-		/// received.
-		case failed(Error)
+	/// Whether this event is a completed event.
+	public var isCompleted: Bool {
+		switch self {
+		case .completed:
+			return true
 
-		/// The signal successfully terminated. No further events will be received.
-		case completed
-
-		/// Event production on the signal has been interrupted. No further events
-		/// will be received.
-		///
-		/// - important: This event does not signify the successful or failed
-		///              completion of the signal.
-		case interrupted
-
-		/// Whether this event is a completed event.
-		public var isCompleted: Bool {
-			switch self {
-			case .completed:
-				return true
-
-			case .value, .failed, .interrupted:
-				return false
-			}
-		}
-
-		/// Whether this event indicates signal termination (i.e., that no further
-		/// events will be received).
-		public var isTerminating: Bool {
-			switch self {
-			case .value:
-				return false
-
-			case .failed, .completed, .interrupted:
-				return true
-			}
-		}
-
-		/// Lift the given closure over the event's value.
-		///
-		/// - important: The closure is called only on `value` type events.
-		///
-		/// - parameters:
-		///   - f: A closure that accepts a value and returns a new value
-		///
-		/// - returns: An event with function applied to a value in case `self` is a
-		///            `value` type of event.
-		public func map<U>(_ f: (Value) -> U) -> Event<U, Error> {
-			switch self {
-			case let .value(value):
-				return .value(f(value))
-
-			case let .failed(error):
-				return .failed(error)
-
-			case .completed:
-				return .completed
-
-			case .interrupted:
-				return .interrupted
-			}
-		}
-
-		/// Lift the given closure over the event's error.
-		///
-		/// - important: The closure is called only on failed type event.
-		///
-		/// - parameters:
-		///   - f: A closure that accepts an error object and returns
-		///        a new error object
-		///
-		/// - returns: An event with function applied to an error object in case
-		///            `self` is a `.Failed` type of event.
-		public func mapError<F>(_ f: (Error) -> F) -> Event<Value, F> {
-			switch self {
-			case let .value(value):
-				return .value(value)
-
-			case let .failed(error):
-				return .failed(f(error))
-
-			case .completed:
-				return .completed
-
-			case .interrupted:
-				return .interrupted
-			}
-		}
-
-		/// Unwrap the contained `value` value.
-		public var value: Value? {
-			if case let .value(value) = self {
-				return value
-			} else {
-				return nil
-			}
-		}
-
-		/// Unwrap the contained `Error` value.
-		public var error: Error? {
-			if case let .failed(error) = self {
-				return error
-			} else {
-				return nil
-			}
+		case .value, .failed, .interrupted:
+			return false
 		}
 	}
+
+	/// Whether this event indicates signal termination (i.e., that no further
+	/// events will be received).
+	public var isTerminating: Bool {
+		switch self {
+		case .value:
+			return false
+
+		case .failed, .completed, .interrupted:
+			return true
+		}
+	}
+
+	/// Lift the given closure over the event's value.
+	///
+	/// - important: The closure is called only on `value` type events.
+	///
+	/// - parameters:
+	///   - f: A closure that accepts a value and returns a new value
+	///
+	/// - returns: An event with function applied to a value in case `self` is a
+	///            `value` type of event.
+	public func map<U>(_ f: (Value) -> U) -> Event<U, Error> {
+		switch self {
+		case let .value(value):
+			return .value(f(value))
+
+		case let .failed(error):
+			return .failed(error)
+
+		case .completed:
+			return .completed
+
+		case .interrupted:
+			return .interrupted
+		}
+	}
+
+	/// Lift the given closure over the event's error.
+	///
+	/// - important: The closure is called only on failed type event.
+	///
+	/// - parameters:
+	///   - f: A closure that accepts an error object and returns
+	///        a new error object
+	///
+	/// - returns: An event with function applied to an error object in case
+	///            `self` is a `.Failed` type of event.
+	public func mapError<F>(_ f: (Error) -> F) -> Event<Value, F> {
+		switch self {
+		case let .value(value):
+			return .value(value)
+
+		case let .failed(error):
+			return .failed(f(error))
+
+		case .completed:
+			return .completed
+
+		case .interrupted:
+			return .interrupted
+		}
+	}
+
+	/// Unwrap the contained `value` value.
+	public var value: Value? {
+		if case let .value(value) = self {
+			return value
+		} else {
+			return nil
+		}
+	}
+
+	/// Unwrap the contained `Error` value.
+	public var error: Error? {
+		if case let .failed(error) = self {
+			return error
+		} else {
+			return nil
+		}
+	}
+}
 
 extension Event where Value: Equatable, Error: Equatable {
 	public static func == (lhs: Event<Value, Error>, rhs: Event<Value, Error>) -> Bool {
--- Sources/Observer.swift
+++ Sources/Observer.swift
@@ -6,121 +6,121 @@
 //  Copyright © 2015 GitHub. All rights reserved.
 //
 
-	/// An Observer is a simple wrapper around a function which can receive Events
-	/// (typically from a Signal).
-	public final class Observer<Value, Error: Swift.Error> {
-		public typealias Action = (Event<Value, Error>) -> Void
-		private let _send: Action
+/// An Observer is a simple wrapper around a function which can receive Events
+/// (typically from a Signal).
+public final class Observer<Value, Error: Swift.Error> {
+	public typealias Action = (Event<Value, Error>) -> Void
+	private let _send: Action
 
-		/// Whether the observer should send an `interrupted` event as it deinitializes.
-		private let interruptsOnDeinit: Bool
+	/// Whether the observer should send an `interrupted` event as it deinitializes.
+	private let interruptsOnDeinit: Bool
 
-		/// An initializer that accepts a closure accepting an event for the
-		/// observer.
-		///
-		/// - parameters:
-		///   - action: A closure to lift over received event.
-		///   - interruptsOnDeinit: `true` if the observer should send an `interrupted`
-		///                         event as it deinitializes. `false` otherwise.
-		internal init(action: @escaping Action, interruptsOnDeinit: Bool) {
-			self._send = action
-			self.interruptsOnDeinit = interruptsOnDeinit
-		}
+	/// An initializer that accepts a closure accepting an event for the
+	/// observer.
+	///
+	/// - parameters:
+	///   - action: A closure to lift over received event.
+	///   - interruptsOnDeinit: `true` if the observer should send an `interrupted`
+	///                         event as it deinitializes. `false` otherwise.
+	internal init(action: @escaping Action, interruptsOnDeinit: Bool) {
+		self._send = action
+		self.interruptsOnDeinit = interruptsOnDeinit
+	}
 
-		/// An initializer that accepts a closure accepting an event for the 
-		/// observer.
-		///
-		/// - parameters:
-		///   - action: A closure to lift over received event.
-		public init(_ action: @escaping Action) {
-			self._send = action
-			self.interruptsOnDeinit = false
-		}
+	/// An initializer that accepts a closure accepting an event for the 
+	/// observer.
+	///
+	/// - parameters:
+	///   - action: A closure to lift over received event.
+	public init(_ action: @escaping Action) {
+		self._send = action
+		self.interruptsOnDeinit = false
+	}
 
-		/// An initializer that accepts closures for different event types.
-		///
-		/// - parameters:
-		///   - value: Optional closure executed when a `value` event is observed.
-		///   - failed: Optional closure that accepts an `Error` parameter when a
-		///             failed event is observed.
-		///   - completed: Optional closure executed when a `completed` event is
-		///                observed.
-		///   - interruped: Optional closure executed when an `interrupted` event is
-		///                 observed.
-		public convenience init(
-			value: ((Value) -> Void)? = nil,
-			failed: ((Error) -> Void)? = nil,
-			completed: (() -> Void)? = nil,
-			interrupted: (() -> Void)? = nil
-		) {
-			self.init { event in
-				switch event {
-				case let .value(v):
-					value?(v)
+	/// An initializer that accepts closures for different event types.
+	///
+	/// - parameters:
+	///   - value: Optional closure executed when a `value` event is observed.
+	///   - failed: Optional closure that accepts an `Error` parameter when a
+	///             failed event is observed.
+	///   - completed: Optional closure executed when a `completed` event is
+	///                observed.
+	///   - interruped: Optional closure executed when an `interrupted` event is
+	///                 observed.
+	public convenience init(
+		value: ((Value) -> Void)? = nil,
+		failed: ((Error) -> Void)? = nil,
+		completed: (() -> Void)? = nil,
+		interrupted: (() -> Void)? = nil
+	) {
+		self.init { event in
+			switch event {
+			case let .value(v):
+				value?(v)
 
-				case let .failed(error):
-					failed?(error)
+			case let .failed(error):
+				failed?(error)
 
-				case .completed:
-					completed?()
+			case .completed:
+				completed?()
 
-				case .interrupted:
-					interrupted?()
-				}
+			case .interrupted:
+				interrupted?()
 			}
 		}
+	}
 
-		internal convenience init(mappingInterruptedToCompleted observer: Observer<Value, Error>) {
-			self.init { event in
-				switch event {
-				case .value, .completed, .failed:
-					observer.send(event)
-				case .interrupted:
-					observer.sendCompleted()
-				}
+	internal convenience init(mappingInterruptedToCompleted observer: Observer<Value, Error>) {
+		self.init { event in
+			switch event {
+			case .value, .completed, .failed:
+				observer.send(event)
+			case .interrupted:
+				observer.sendCompleted()
 			}
 		}
+	}
 
-		deinit {
-			if interruptsOnDeinit {
-				// Since `Signal` would ensure that only one terminal event would ever be
-				// sent for any given `Signal`, we do not need to assert any condition
-				// here.
-				_send(.interrupted)
-			}
-		}
-
-		/// Puts an event into `self`.
-		public func send(_ event: Event<Value, Error>) {
-			_send(event)
-		}
-
-		/// Puts a `value` event into `self`.
-		///
-		/// - parameters:
-		///   - value: A value sent with the `value` event.
-		public func send(value: Value) {
-			_send(.value(value))
-		}
-
-		/// Puts a failed event into `self`.
-		///
-		/// - parameters:
-		///   - error: An error object sent with failed event.
-		public func send(error: Error) {
-			_send(.failed(error))
-		}
-
-		/// Puts a `completed` event into `self`.
-		public func sendCompleted() {
-			_send(.completed)
-		}
-
-		/// Puts an `interrupted` event into `self`.
-		public func sendInterrupted() {
+	deinit {
+		if interruptsOnDeinit {
+			// Since `Signal` would ensure that only one terminal event would ever be
+			// sent for any given `Signal`, we do not need to assert any condition
+			// here.
 			_send(.interrupted)
 		}
 	}
+
+	/// Puts an event into `self`.
+	public func send(_ event: Event<Value, Error>) {
+		_send(event)
+	}
+
+	/// Puts a `value` event into `self`.
+	///
+	/// - parameters:
+	///   - value: A value sent with the `value` event.
+	public func send(value: Value) {
+		_send(.value(value))
+	}
+
+	/// Puts a failed event into `self`.
+	///
+	/// - parameters:
+	///   - error: An error object sent with failed event.
+	public func send(error: Error) {
+		_send(.failed(error))
+	}
+
+	/// Puts a `completed` event into `self`.
+	public func sendCompleted() {
+		_send(.completed)
+	}
+
+	/// Puts an `interrupted` event into `self`.
+	public func sendInterrupted() {
+		_send(.interrupted)
+	}
+}
 
 /// FIXME: Cannot be placed in `Deprecations+Removal.swift` if compiling with
 ///        Xcode 9.2.
