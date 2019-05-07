1ea6fd8f8e056d5ab002946bfdd591b2318676b7
Jakub Olejník <olejnjak@gmail.com>
2019-05-07 13:54:22 +0200

    Add tests for static `SignalProducer.or` with collection of producers

--- Tests/ReactiveSwiftTests/SignalProducerSpec.swift
+++ Tests/ReactiveSwiftTests/SignalProducerSpec.swift
@@ -3014,6 +3014,25 @@ class SignalProducerSpec: QuickSpec {
 					expect(value).to(beTrue())
 				}
 			}
+			
+			it("should emit true when at least one of the producers in array emits true") {
+				let producer1 = SignalProducer<Bool, Never> { observer, _ in
+					observer.send(value: true)
+					observer.sendCompleted()
+				}
+				let producer2 = SignalProducer<Bool, Never> { observer, _ in
+					observer.send(value: false)
+					observer.sendCompleted()
+				}
+				let producer3 = SignalProducer<Bool, Never> { observer, _ in
+					observer.send(value: false)
+					observer.sendCompleted()
+				}
+				
+				SignalProducer.or([producer1, producer2, producer3]).startWithValues { value in
+					expect(value).to(beTrue())
+				}
+			}
 
 			it("should emit false when both producers emits false") {
 				let producer1 = SignalProducer<Bool, Never> { observer, _ in
@@ -3029,6 +3048,25 @@ class SignalProducerSpec: QuickSpec {
 					expect(value).to(beFalse())
 				}
 			}
+			
+			it("should emit false when all producers in array emit false") {
+				let producer1 = SignalProducer<Bool, Never> { observer, _ in
+					observer.send(value: false)
+					observer.sendCompleted()
+				}
+				let producer2 = SignalProducer<Bool, Never> { observer, _ in
+					observer.send(value: false)
+					observer.sendCompleted()
+				}
+				let producer3 = SignalProducer<Bool, Never> { observer, _ in
+					observer.send(value: false)
+					observer.sendCompleted()
+				}
+				
+				SignalProducer.or([producer1, producer2, producer3]).startWithValues { value in
+					expect(value).to(beFalse())
+				}
+			}
 
 			it("should work the same way when using signal instead of a producer") {
 				let producer1 = SignalProducer<Bool, Never> { observer, _ in
@@ -3040,12 +3078,31 @@ class SignalProducerSpec: QuickSpec {
 					expect(value).to(beTrue())
 				}
 				observer2.send(value: true)
-
+				
 				observer2.sendCompleted()
+			}
+			
+			it("should work the same way when using array of signals instead of an array of producers") {
+				let (signal1, observer1) = Signal<Bool, Never>.pipe()
+				let (signal2, observer2) = Signal<Bool, Never>.pipe()
+				let (signal3, observer3) = Signal<Bool, Never>.pipe()
+				let arrayOfSignals = [signal1, signal2, signal3]
+				
+				SignalProducer.or(arrayOfSignals).startWithValues { value in
+					expect(value).to(beTrue())
+				}
+				observer1.send(value: true)
+				observer1.sendCompleted()
+				observer2.send(value: true)
+				observer2.sendCompleted()
+				observer3.send(value: true)
+				observer3.sendCompleted()
 			}
 
 			it("should be able to fallback to SignalProducer for contextual lookups") {
 				_ = SignalProducer<Bool, Never>.empty
+					.or(.init(value: true))
+				_ = SignalProducer<Bool, Never>
 					.or(.init(value: true))
 			}
 		}
