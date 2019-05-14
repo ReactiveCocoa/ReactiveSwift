bd0f9d984bfd0049bdd72c853971ce661bc08dd4
Jakub Olejník <olejnjak@gmail.com>
2019-05-14 15:07:27 +0200

    Rename static `Property.or` to `any`

--- Sources/Property.swift
+++ Sources/Property.swift
@@ -439,7 +439,7 @@ extension PropertyProtocol where Value == Bool {
 	///   - properties: Collection of properties to be combined.
 	///
 	/// - returns: A property that contains the logial OR results.
-	public static func or<P: PropertyProtocol, Properties: Collection>(_ properties: Properties) -> Property<Value> where P.Value == Value, Properties.Element == P {
+	public static func any<P: PropertyProtocol, Properties: Collection>(_ properties: Properties) -> Property<Value> where P.Value == Value, Properties.Element == P {
 		return Property(initial: properties.map { $0.value }.reduce(false) { $0 || $1 }, then: SignalProducer.or(properties))
 	}
 }
