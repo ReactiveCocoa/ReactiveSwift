ea63d3a3b631daef292bd1da2b513f715b93de4f
Łukasz Mróz <thesunshinejr@gmail.com>
2019-09-19 15:48:47 +0200

    Add option to skip building testing libraries for SPM users

--- Package.swift
+++ Package.swift
@@ -1,18 +1,31 @@
 // swift-tools-version:5.0
 import PackageDescription
+import class Foundation.ProcessInfo
+
+let shouldTest = ProcessInfo.processInfo.environment["TEST"] == "1"
+
+func resolveDependencies() -> [Package.Dependency] {
+    guard shouldTest else { return [] }
+
+    return [
+        .package(url: "https://github.com/Quick/Quick.git", from: "2.0.0"),
+        .package(url: "https://github.com/Quick/Nimble.git", from: "8.0.0"),
+    ]
+}
+
+func resolveTargets() -> [Target] {
+    let baseTarget = Target.target(name: "ReactiveSwift", dependencies: [], path: "Sources")
+    let testTarget = Target.testTarget(name: "ReactiveSwiftTests", dependencies: ["ReactiveSwift", "Quick", "Nimble"])
+
+    return shouldTest ? [baseTarget, testTarget] : [baseTarget]
+}
 
 let package = Package(
     name: "ReactiveSwift",
     products: [
         .library(name: "ReactiveSwift", targets: ["ReactiveSwift"]),
     ],
-    dependencies: [
-        .package(url: "https://github.com/Quick/Quick.git", from: "2.0.0"),
-        .package(url: "https://github.com/Quick/Nimble.git", from: "8.0.0"),
-    ],
-    targets: [
-        .target(name: "ReactiveSwift", dependencies: [], path: "Sources"),
-        .testTarget(name: "ReactiveSwiftTests", dependencies: ["ReactiveSwift", "Quick", "Nimble"]),
-    ],
+    dependencies: resolveDependencies(),
+    targets: resolveTargets(),
     swiftLanguageVersions: [.v5]
 )
