d7b1a5d7d3b8982bffe3f2043157f4def68fe385
Anders Ha <anders@andersha.com>
2020-04-26 21:37:02 +0100

    Remove `Cancellable` conformances.

--- Sources/CombineInteroperability/Utilities.swift
+++ Sources/CombineInteroperability/Utilities.swift
@@ -4,29 +4,9 @@ import Combine
 extension Lifetime {
 	@available(macOS 10.15, iOS 13.0, tvOS 13.0, macCatalyst 13.0, watchOS 6.0, *)
 	@discardableResult
-	public static func += <C: Cancellable>(lhs: Lifetime, rhs: C) -> Disposable? {
-		lhs.observeEnded(rhs.cancel)
-	}
-}
-
-@available(macOS 10.15, iOS 13.0, tvOS 13.0, macCatalyst 13.0, watchOS 6.0, *)
-extension AnyDisposable: Cancellable {
-	public func cancel() {
-		dispose()
-	}
-}
-
-@available(macOS 10.15, iOS 13.0, tvOS 13.0, macCatalyst 13.0, watchOS 6.0, *)
-extension SerialDisposable: Cancellable {
-	public func cancel() {
-		dispose()
-	}
-}
-
-@available(macOS 10.15, iOS 13.0, tvOS 13.0, macCatalyst 13.0, watchOS 6.0, *)
-extension CompositeDisposable: Cancellable {
-	public func cancel() {
-		dispose()
+	public static func += <C: Cancellable>(lhs: Lifetime, rhs: C?) -> Disposable? {
+		rhs.flatMap { lhs.observeEnded($0.cancel) }
 	}
 }
 #endif
+
