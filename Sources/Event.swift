384b4643044d1124f84c5374d7f510ee0d368b14
Anders Ha <anders@andersha.com>
2021-01-11 13:27:44 +0000

    Migrate six operators to the new abstraction. (#814)
    * Collect, CombinePrevious, SkipRepeats, UniqueValues
    
    * Reduce, ScanMap

--- Sources/Event.swift
+++ Sources/Event.swift
@@ -325,38 +325,6 @@ extension Signal.Event where Value: OptionalProtocol {
 	}
 }
 
-/// A reference type which wraps an array to auxiliate the collection of values
-/// for `collect` operator.
-private final class CollectState<Value> {
-	var values: [Value] = []
-
-	/// Collects a new value.
-	func append(_ value: Value) {
-		values.append(value)
-	}
-
-	/// Check if there are any items remaining.
-	///
-	/// - note: This method also checks if there weren't collected any values
-	///         and, in that case, it means an empty array should be sent as the
-	///         result of collect.
-	var isEmpty: Bool {
-		/// We use capacity being zero to determine if we haven't collected any
-		/// value since we're keeping the capacity of the array to avoid
-		/// unnecessary and expensive allocations). This also guarantees
-		/// retro-compatibility around the original `collect()` operator.
-		return values.isEmpty && values.capacity > 0
-	}
-
-	/// Removes all values previously collected if any.
-	func flush() {
-		// Minor optimization to avoid consecutive allocations. Can
-		// be useful for sequences of regular or similar size and to
-		// track if any value was ever collected.
-		values.removeAll(keepingCapacity: true)
-	}
-}
-
 extension Signal.Event {
 	internal static var collect: Transformation<[Value], Error> {
 		return collect { _, _ in false }
@@ -368,54 +336,14 @@ extension Signal.Event {
 	}
 
 	internal static func collect(_ shouldEmit: @escaping (_ collectedValues: [Value]) -> Bool) -> Transformation<[Value], Error> {
-		return { action, _ in
-			let state = CollectState<Value>()
-
-			return Signal.Observer { event in
-				switch event {
-				case let .value(value):
-					state.append(value)
-					if shouldEmit(state.values) {
-						action(.value(state.values))
-						state.flush()
-					}
-				case .completed:
-					if !state.isEmpty {
-						action(.value(state.values))
-					}
-					action(.completed)
-				case let .failed(error):
-					action(.failed(error))
-				case .interrupted:
-					action(.interrupted)
-				}
-			}
+		return { downstream, _ in
+			Operators.Collect(downstream: downstream, shouldEmit: shouldEmit)
 		}
 	}
 
 	internal static func collect(_ shouldEmit: @escaping (_ collected: [Value], _ latest: Value) -> Bool) -> Transformation<[Value], Error> {
-		return { action, _ in
-			let state = CollectState<Value>()
-
-			return Signal.Observer { event in
-				switch event {
-				case let .value(value):
-					if shouldEmit(state.values, value) {
-						action(.value(state.values))
-						state.flush()
-					}
-					state.append(value)
-				case .completed:
-					if !state.isEmpty {
-						action(.value(state.values))
-					}
-					action(.completed)
-				case let .failed(error):
-					action(.failed(error))
-				case .interrupted:
-					action(.interrupted)
-				}
-			}
+		return { downstream, _ in
+			Operators.Collect(downstream: downstream, shouldEmit: shouldEmit)
 		}
 	}
 
@@ -424,83 +352,26 @@ extension Signal.Event {
 	/// `nil` literal would be materialized as `Optional<Value>.none` instead of `Value`,
 	/// thus changing the semantic.
 	internal static func combinePrevious(initial: Value?) -> Transformation<(Value, Value), Error> {
-		return { action, _ in
-			var previous = initial
-
-			return Signal.Observer { event in
-				switch event {
-				case let .value(value):
-					if let previous = previous {
-						action(.value((previous, value)))
-					}
-					previous = value
-				case .completed:
-					action(.completed)
-				case let .failed(error):
-					action(.failed(error))
-				case .interrupted:
-					action(.interrupted)
-				}
-			}
+		return { downstream, _ in
+			Operators.CombinePrevious(downstream: downstream, initial: initial)
 		}
 	}
 
 	internal static func skipRepeats(_ isEquivalent: @escaping (Value, Value) -> Bool) -> Transformation<Value, Error> {
-		return { action, _ in
-			var previous: Value?
-
-			return Signal.Observer { event in
-				switch event {
-				case let .value(value):
-					if let previous = previous, isEquivalent(previous, value) {
-						return
-					}
-					previous = value
-					fallthrough
-				case .completed, .interrupted, .failed:
-					action(event)
-				}
-			}
+		return { downstream, _ in
+			Operators.SkipRepeats(downstream: downstream, isEquivalent: isEquivalent)
 		}
 	}
 
 	internal static func uniqueValues<Identity: Hashable>(_ transform: @escaping (Value) -> Identity) -> Transformation<Value, Error> {
-		return { action, _ in
-			var seenValues: Set<Identity> = []
-
-			return Signal.Observer { event in
-				switch event {
-				case let .value(value):
-					let identity = transform(value)
-					let (inserted, _) = seenValues.insert(identity)
-					if inserted {
-						fallthrough
-					}
-
-				case .failed, .completed, .interrupted:
-					action(event)
-				}
-			}
+		return { downstream, _ in
+			Operators.UniqueValues(downstream: downstream, extract: transform)
 		}
 	}
 
 	internal static func reduce<U>(into initialResult: U, _ nextPartialResult: @escaping (inout U, Value) -> Void) -> Transformation<U, Error> {
-		return { action, _ in
-			var accumulator = initialResult
-
-			return Signal.Observer { event in
-				switch event {
-				case let .value(value):
-					nextPartialResult(&accumulator, value)
-				case .completed:
-					action(.value(accumulator))
-					action(.completed)
-				case .interrupted:
-					action(.interrupted)
-				case let .failed(error):
-					action(.failed(error))
-				}
-			}
+		return { downstream, _ in
+			Operators.Reduce(downstream: downstream, initial: initialResult, nextPartialResult: nextPartialResult)
 		}
 	}
 
@@ -520,22 +391,8 @@ extension Signal.Event {
 	}
 
 	internal static func scanMap<State, U>(into initialState: State, _ next: @escaping (inout State, Value) -> U) -> Transformation<U, Error> {
-		return { action, _ in
-			var accumulator = initialState
-
-			return Signal.Observer { event in
-				switch event {
-				case let .value(value):
-					let output = next(&accumulator, value)
-					action(.value(output))
-				case .completed:
-					action(.completed)
-				case .interrupted:
-					action(.interrupted)
-				case let .failed(error):
-					action(.failed(error))
-				}
-			}
+		return { downstream, _ in
+			Operators.ScanMap(downstream: downstream, initial: initialState, next: next)
 		}
 	}
 
