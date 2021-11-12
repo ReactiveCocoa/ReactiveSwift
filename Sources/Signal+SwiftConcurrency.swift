73a5073c6c8952cb6678c6931deac451d7745dd9
Marco Cancellieri <marco.cancellieri@traderepublic.com>
2021-11-12 10:46:20 +0100

    Use tabs instead of spaces to align with rest of codebase

--- Sources/Signal+SwiftConcurrency.swift
+++ Sources/Signal+SwiftConcurrency.swift
@@ -10,43 +10,43 @@ import Foundation
 
 @available(macOS 12, iOS 15, watchOS 8, tvOS 15, *)
 extension Signal {
-    public var asyncThrowingStream: AsyncThrowingStream<Value, Swift.Error> {
-        AsyncThrowingStream<Value, Swift.Error> { continuation in
-            let disposable = observe { event in
-                switch event {
-                case .value(let value):
-                    continuation.yield(value)
-                case .completed, .interrupted:
-                    continuation.finish()
-                case .failed(let error):
-                    continuation.finish(throwing: error)
-                }
-            }
-            continuation.onTermination = { @Sendable termination in
-                disposable?.dispose()
-            }
-        }
-    }
+	public var asyncThrowingStream: AsyncThrowingStream<Value, Swift.Error> {
+		AsyncThrowingStream<Value, Swift.Error> { continuation in
+			let disposable = observe { event in
+				switch event {
+				case .value(let value):
+					continuation.yield(value)
+				case .completed, .interrupted:
+					continuation.finish()
+				case .failed(let error):
+					continuation.finish(throwing: error)
+				}
+			}
+			continuation.onTermination = { @Sendable termination in
+				disposable?.dispose()
+			}
+		}
+	}
 }
 
 extension Signal where Error == Never {
-    @available(macOS 12, iOS 15, watchOS 8, tvOS 15, *)
-    public var asyncStream: AsyncStream<Value> {
-        AsyncStream<Value> { continuation in
-            let disposable = observe { event in
-                switch event {
-                case .value(let value):
-                    continuation.yield(value)
-                case .completed, .interrupted:
-                    continuation.finish()
-                case .failed:
-                    fatalError("Never is impossible to construct")
-                }
-            }
-            continuation.onTermination = { @Sendable termination in
-                disposable?.dispose()
-            }
-        }
-    }
+	@available(macOS 12, iOS 15, watchOS 8, tvOS 15, *)
+	public var asyncStream: AsyncStream<Value> {
+		AsyncStream<Value> { continuation in
+			let disposable = observe { event in
+				switch event {
+				case .value(let value):
+					continuation.yield(value)
+				case .completed, .interrupted:
+					continuation.finish()
+				case .failed:
+					fatalError("Never is impossible to construct")
+				}
+			}
+			continuation.onTermination = { @Sendable termination in
+				disposable?.dispose()
+			}
+		}
+	}
 }
 #endif
--- Sources/SignalProducer+SwiftConcurrency.swift
+++ Sources/SignalProducer+SwiftConcurrency.swift
@@ -10,43 +10,43 @@ import Foundation
 
 @available(macOS 12, iOS 15, watchOS 8, tvOS 15, *)
 extension SignalProducer {
-    public var asyncThrowingStream: AsyncThrowingStream<Value, Swift.Error> {
-        AsyncThrowingStream<Value, Swift.Error> { continuation in
-            let disposable = start { event in
-                switch event {
-                case .value(let value):
-                    continuation.yield(value)
-                case .completed, .interrupted:
-                    continuation.finish()
-                case .failed(let error):
-                    continuation.finish(throwing: error)
-                }
-            }
-            continuation.onTermination = { @Sendable _ in
-                disposable.dispose()
-            }
-        }
-    }
+	public var asyncThrowingStream: AsyncThrowingStream<Value, Swift.Error> {
+		AsyncThrowingStream<Value, Swift.Error> { continuation in
+			let disposable = start { event in
+				switch event {
+				case .value(let value):
+					continuation.yield(value)
+				case .completed, .interrupted:
+					continuation.finish()
+				case .failed(let error):
+					continuation.finish(throwing: error)
+				}
+			}
+			continuation.onTermination = { @Sendable _ in
+				disposable.dispose()
+			}
+		}
+	}
 }
 
 @available(macOS 12, iOS 15, watchOS 8, tvOS 15, *)
 extension SignalProducer where Error == Never {
-    public var asyncStream: AsyncStream<Value> {
-        AsyncStream<Value> { continuation in
-            let disposable = start { event in
-                switch event {
-                case .value(let value):
-                    continuation.yield(value)
-                case .completed, .interrupted:
-                    continuation.finish()
-                case .failed:
-                    fatalError("Never is impossible to construct")
-                }
-            }
-            continuation.onTermination = { @Sendable _ in
-                disposable.dispose()
-            }
-        }
-    }
+	public var asyncStream: AsyncStream<Value> {
+		AsyncStream<Value> { continuation in
+			let disposable = start { event in
+				switch event {
+				case .value(let value):
+					continuation.yield(value)
+				case .completed, .interrupted:
+					continuation.finish()
+				case .failed:
+					fatalError("Never is impossible to construct")
+				}
+			}
+			continuation.onTermination = { @Sendable _ in
+				disposable.dispose()
+			}
+		}
+	}
 }
 #endif
